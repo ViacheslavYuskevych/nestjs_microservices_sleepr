47137cf370e72563652be5aacd461dcb5f8f4da7
slavko olich <slavko.olich@gmail.com>
2025-05-06 12:16:00 +0300

    reservations crud

--- apps/reservations/src/reservations.controller.ts
+++ apps/reservations/src/reservations.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { UpdateReservationDto } from './dto/update-reservation.dto';
 
-@Controller()
+@Controller('reservation')
 export class ReservationsController {
-  constructor(private readonly reservationsService: ReservationsService) {}
+  constructor(private readonly reservationService: ReservationsService) {}
+
+  @Post()
+  create(@Body() createReservationDto: CreateReservationDto) {
+    return this.reservationService.create(createReservationDto);
+  }
 
   @Get()
-  getHello(): string {
-    return this.reservationsService.getHello();
+  findAll() {
+    return this.reservationService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.reservationService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateReservationDto: UpdateReservationDto,
+  ) {
+    return this.reservationService.update(+id, updateReservationDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.reservationService.remove(+id);
   }
 }
--- apps/reservations/src/reservations.service.ts
+++ apps/reservations/src/reservations.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { ReservationsRepository } from './reservations.repository';
 
 @Injectable()
 export class ReservationsService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly repository: ReservationsRepository) {}
+
+  create(createReservationDto: CreateReservationDto) {
+    return this.repository.create({
+      ...createReservationDto,
+      timestamp: new Date(),
+      userId: '123',
+    });
+  }
+
+  findAll() {
+    return this.repository.find({});
+  }
+
+  findOne(_id: number) {
+    return this.repository.findOne({ _id });
+  }
+
+  update(_id: number, updateReservationDto: UpdateReservationDto) {
+    return this.repository.findOneAndUpdate(
+      { _id },
+      { $set: updateReservationDto },
+    );
+  }
+
+  remove(_id: number) {
+    return this.repository.findOneAndDelete({ _id });
   }
 }
